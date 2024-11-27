234c302ecab4642d15b854449f860b2babe2adef
saeed-mohammad <sk.saeed008@gmail.com>
2024-11-27 22:34:41 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by saeed shaikh on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
