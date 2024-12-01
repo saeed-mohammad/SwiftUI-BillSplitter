1058e8ba25f404924f8a2d587cb98df18af4e7ef
saeed-mohammad <sk.saeed008@gmail.com>
2024-12-01 23:32:09 +0530

    fixes bug and improve ui

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,8 +11,9 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numOfPeope = 2
     @State private var selectedPercentage = 0
+    @FocusState private var isFocused
     
-    let tipPercentage = [0, 10, 15, 20, 25, 30]
+//    let tipPercentage = [0, 10, 15, 20, 25, 30]
     
     var currencyFormatter: NumberFormatter {
         let formatter = NumberFormatter()
@@ -21,17 +22,28 @@ struct ContentView: View {
         return formatter
     }
     
-    private func hideKeyboard(){
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
+    //    private func hideKeyboard(){
+    //        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    //    }
     
-    private func calculation() -> Double {
-        guard numOfPeope > 0 else { return 0 }
+    var AmountPerPerson: Double{
         let finalAmount = checkAmount
         let tipAmount = checkAmount * (Double(selectedPercentage) / 100.0)
         let perPerson = (finalAmount+tipAmount) / Double(numOfPeope)
         return perPerson
     }
+    var totalAmountAfterTip: Double{
+        let finalAmount = checkAmount
+        let tipAmount = checkAmount * (Double(selectedPercentage) / 100.0)
+        return finalAmount + tipAmount
+    }
+    //    private func calculation() -> Double {
+    //        guard numOfPeope > 0 else { return 0 }
+    //        let finalAmount = checkAmount
+    //        let tipAmount = checkAmount * (Double(selectedPercentage) / 100.0)
+    //        let perPerson = (finalAmount+tipAmount) / Double(numOfPeope)
+    //        return perPerson
+    //    }
     
     var body: some View {
         NavigationStack{
@@ -40,23 +52,38 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", value: $checkAmount, formatter: currencyFormatter)
                         .keyboardType(.decimalPad)
-                    Picker("Number of People", selection: $numOfPeope){
-                        ForEach(1...15, id: \.self){
-                            Text("People \($0)")
-                        }
+                        .focused($isFocused)
+                    
+                    if #available(iOS 17.0, *) {
+                        Picker("Number of People", selection: $numOfPeope){
+                            ForEach(1...15, id: \.self){
+                                Text("People \($0)")
+                            }
+                        }.pickerStyle(.wheel)
+                    } else {
+                        // Fallback on earlier versions
                     }
                 }
                 
                 Section("How much do you want to tip?"){
                     Picker("Tip Percentage", selection: $selectedPercentage){
-                        ForEach(tipPercentage, id: \.self){percentage in
-                            Text("\(percentage)%")
+                        //                        ForEach(tipPercentage, id: \.self){percentage in
+                        //                            Text("\(percentage)%")
+                        //                        }
+                        ForEach(0...100, id: \.self){
+                            Text("\($0)%")
                         }
                     }
-                }.pickerStyle(SegmentedPickerStyle())
+                }
+                //                .pickerStyle(.segmented)
+                .pickerStyle(.navigationLink)
                 
-                Section {
-                    Text("Total Amount per Person: \(calculation(), specifier: "%.2f")")
+                Section ("Total Amount After Including Tip"){
+                    Text("\(totalAmountAfterTip, specifier: "%.2f")")
+                }
+                Section ("Amount per person"){
+                    //                    Text("Total Amount per Person: \(calculation(), specifier: "%.2f")")
+                    Text("\(AmountPerPerson, specifier: "%.2f")")
                 }
                 
                 
@@ -65,6 +92,13 @@ struct ContentView: View {
 //                hideKeyboard() // Dismiss keyboard when tapping anywhere else
 //            }
             .navigationTitle("WeSplit")
+            .toolbar{
+                if isFocused {
+                    Button("Done"){
+                        isFocused = false
+                    }
+                }
+            }
         }
     }
 }
